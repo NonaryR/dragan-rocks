0a7600252c6615643ece3af05a271b8e4be8aa81
rustam.gilaztdinov <hsec@ro.ru>
2017-10-12 00:15:55 +0300

    first commit

---
+++ src/dragan_rocks/clojure_numerics_part_1.clj
@@ -0,0 +1,51 @@
+(ns dragan-rocks.clojure-numerics-part-1
+  (:require [uncomplicate.neanderthal
+             [native :refer [dv dge fge dtr native-float]]
+             [opencl :as ocl]
+             [core :refer [copy copy! row submatrix scal! transfer! transfer mrows ncols nrm2 mm cols view-tr xpy scal axpy! zero]]
+             [real :refer [entry entry!]]
+             [linalg :refer [trf tri det]]]
+           [uncomplicate.commons.core :refer [with-release]]
+           [uncomplicate.clojurecl.core :as clojurecl]
+           [uncomplicate.clojurecl.info :as info]))
+
+#_(map info/info (clojurecl/platforms))
+
+#_(map info/info (clojurecl/devices (first (clojurecl/platforms))))
+
+
+(def v1 (dv -1 2 5.2 0))
+
+(def v2 (dv (range 22)))
+
+(def v3 (dv -2 -3 1 0))
+
+;; сумма v1 v3
+(xpy v1 v3)
+
+;; скалярное произведение
+(scal 2.5 v1)
+
+;; скалярное первого вектора и сумма со вторым
+(axpy! 2.5 v1 v3)
+
+;; 7 нулей
+(dv 7)
+
+;; zeros-like
+(zero v2)
+
+(dge 3 2 [1 2 3 4 5 6] {:order :row})
+
+(row (dge 2 3 (range 6)) 1)
+
+(let [a (dge 2 3 (range 6))
+      b (submatrix a 0 1 1 2)]
+  (scal! 100 b)
+  a)
+
+(let [a (dge 3 2 (range 6))
+      b (dge 3 2)]
+  (copy! a b)
+  (entry! a 1 1 800)
+  (copy b))
