0b1f221e88ed23249bcd58c7edc5f8112a97462e
rustam.gilaztdinov <hsec@ro.ru>
2017-12-17 23:55:03 +0300

    perfomance testing

--- src/dragan_rocks/nn.clj
+++ src/dragan_rocks/nn.clj
@@ -37,19 +37,22 @@
 (defn update-weights [weights layer next-layer-grad]
   (m/add! weights (m/mmul (m/transpose layer) next-layer-grad)))
 
-(let [out-count (first (m/shape output-data))
-      synaptic-weight-0 (synaptic-weight 3 4)
-      synaptic-weight-1 (synaptic-weight 4 1)]
-  (dotimes [i 60000]
-    (let [layer-0 input-data
-          layer-1 (feed-forward layer-0 synaptic-weight-0)
+(time
+ (let [out-count (first (m/shape output-data))
+       layer-0 input-data
+       synaptic-weight-0 (synaptic-weight 3 4)
+       synaptic-weight-1 (synaptic-weight 4 1)
+       final-error (partial m/add output-data)
+       learn-log (fn [l-2-er] (println (/ (m/ereduce + (m/abs l-2-er)) out-count)))]
+  (dotimes [i 600000]
+    (let [layer-1 (feed-forward layer-0 synaptic-weight-0)
           layer-2 (feed-forward layer-1 synaptic-weight-1)
-          layer-2-error (m/add output-data (m/negate layer-2))
+          layer-2-error (final-error (m/negate layer-2))
           layer-2-grad (backprop-gradient layer-2 layer-2-error)
           layer-1-error (m/mmul layer-2-grad (m/transpose synaptic-weight-1))
           layer-1-grad (backprop-gradient layer-1 layer-1-error)
           synaptic-weight-1 (update-weights synaptic-weight-1 layer-1 layer-2-grad)
           synaptic-weight-0 (update-weights synaptic-weight-0 layer-0 layer-1-grad)]
-      (when (== 0 (rem i 10000))
-        (println (/ (m/ereduce + (m/abs layer-2-error)) out-count))
-        (m/pm (m/logistic (m/mmul (m/matrix [0 1 1]) synaptic-weight-0)))))))
+      (when (== 0 (rem i 100000))
+        (learn-log layer-2-error)
+        #_(m/pm (m/logistic (m/mmul (m/matrix [0 1 1]) synaptic-weight-0))))))))
