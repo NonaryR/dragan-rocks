4e82d5ec964d66a8853defa9893d2e5fbfbc02a6
rustam.gilaztdinov <hsec@ro.ru>
2017-12-18 23:53:41 +0300

    deps

--- project.clj
+++ project.clj
@@ -2,14 +2,12 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [uncomplicate/clojurecl "0.7.1"]
-                 [uncomplicate/neanderthal "0.17.0"
+                 [uncomplicate/clojurecl "0.7.2"]
+                 [uncomplicate/neanderthal "0.17.2"
                   :exclusions [org.clojure/tools.reader
                                org.clojure/tools.analyzer.jvm
                                uncomplicate/commons
                                org.clojure/core.async]]
                  [net.mikera/core.matrix "0.61.0"]
-                 [uncomplicate/fluokitten "0.6.0"]
                  [org.clojure/core.async "0.3.465"]
-                 [org.clojure/math.numeric-tower "0.0.4"]
                  [denisovan "0.1.0-SNAPSHOT"]])
